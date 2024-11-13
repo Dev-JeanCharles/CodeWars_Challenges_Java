af10f70d56963a45b6444c3f3fb92060caa451a2
Dev-JeanCharles <jeancduasan@gmail.com>
2024-11-13 02:02:39 -0300

    :sparkles: feat: Challenges completed

---
+++ Fundamentals/src/Kata.java
@@ -0,0 +1,10 @@
+public class Kata {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
